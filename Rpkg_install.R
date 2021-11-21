957c02d511e898d3a518176142329b21a46fd927
Mark Coggeshall <mark.coggeshall@gmail.com>
2021-11-20 19:23:28 -0500

    Permit multi-core builds of R pkgs.

--- Rpkg_install.R
+++ Rpkg_install.R
@@ -2,9 +2,8 @@
 
 pkgs <- read.csv('rpkgs.csv', header=TRUE)
 pkgvec <- as.vector(pkgs$Package)
-library('RODBC')
-withCallingHandlers(install.packages(c(pkgvec)),
+withCallingHandlers(install.packages(c(pkgvec), Ncpus = 4L),
                     warning=function(w) stop(w))
-options(blogdown.hugo.dir='/usr/local/bin/')
+options(blogdown.hugo.dir='/usr/local/')
 blogdown::install_hugo()
 tinytex::install_tinytex()
